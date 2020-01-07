62c1ef06fe12eccfc369fab1ea33a0ea02e310a6
AlbertoGalvan001 <54903554+AlbertoGalvan001@users.noreply.github.com>
2020-01-06 22:41:02 -0500

    dependencies and user migration created

---
+++ data/migrations/20200106214527_users.js
@@ -0,0 +1,22 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('users', tbl => {
+        //ID
+        tbl.increments();
+
+        //Strings
+        tbl.string('username', 128)
+            .notNullable();
+
+        tbl.string('email', 128)
+            .notNullable();
+
+        tbl.string('password', 128)
+            .notNullable();
+    });
+
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('users');
+};
